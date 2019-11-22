edbc15b451bf3d4739ccda533114b3628e8b410b
Greg Osuri <me@gregosuri.com>
2019-11-21 22:53:40 -0800

    ...

--- layer0/packet/common.tf
+++ layer0/packet/common.tf
@@ -1,18 +1,3 @@
-variable "packet_os" {
-  type    = "string"
-  default = "ubuntu_18_04"
-}
-
-variable "packet_plan" {
-  type    = "string"
-  default = "c2.medium.x86"
-}
-
-variable "packet_facility" {
-  type    = "string"
-  default = "sjc1"
-}
-
 provider "packet" {
   version    = "~> 2.4"
   auth_token = "${var.packet_api_token}"
---
+++ layer0/packet/vars.tf
@@ -0,0 +1,40 @@
+# Required variables
+variable "machine_zone" {
+  type = "string"
+}
+
+variable "stack_zone" {
+  type = "string"
+}
+
+variable "packet_api_token" {
+  type = "string"
+}
+
+variable "packet_project_id" {
+  type = "string"
+}
+
+variable "cloudflare_api_token" {
+  type = "string"
+}
+
+variable "machine_name" {
+  type    = "string"
+  default = "akash"
+}
+
+variable "packet_os" {
+  type    = "string"
+  default = "ubuntu_18_04"
+}
+
+variable "packet_plan" {
+  type    = "string"
+  default = "c1.small.x86"
+}
+
+variable "packet_facility" {
+  type    = "string"
+  default = "sjc1"
+}
