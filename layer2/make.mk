877b0d385fd2abcee173af56cf97a0b87834c519
Artur Troian <troian.ap@gmail.com>
2020-05-19 22:20:55 -0400

    fix: update helm cli on layer2 to meet v3
    fixes #36
    
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- layer2/make.mk
+++ layer2/make.mk
@@ -2,7 +2,7 @@ L2DIR 				?= $(PWD)
 L2_NAMESPACE 	= monitoring
 L2_DOMAIN 		= $(HOST)
 
-layer2-install: monitoring-ns-create prometheus-install grafana-install metrics-install
+layer2-install: helm-repo-update monitoring-ns-create prometheus-install grafana-install metrics-install
 
 layer2-remove: prometheus-remove grafana-remove metrics-remove monitoring-ns-remove
 
@@ -20,7 +20,11 @@ layer2-remove.%:
 layer2-remove-all:
 	$(foreach host, $(ALL_HOSTS), $(MAKE) layer2-remove.$(host))
 
-.PHONY: .PHONY layer2-install layer2-remove layer2-install.% layer2-install-all layer2-remove.% layer2-remove-all layer2-check.% layer2-check
+.PHONY: .PHONY helm-repo-update layer2-install layer2-remove layer2-install.% layer2-install-all layer2-remove.% layer2-remove-all layer2-check.% layer2-check
+
+helm-repo-update:
+	helm repo add stable https://kubernetes-charts.storage.googleapis.com
+	helm repo update
 
 monitoring-ns-create:
 	$(KCTL) create namespace $(L2_NAMESPACE) || true
@@ -29,28 +33,28 @@ monitoring-ns-remove:
 	$(KCTL) delete namespace $(L2_NAMESPACE) || true
 
 metrics-install:
-	$(KC) helm install --namespace $(L2_NAMESPACE) stable/metrics-server -n metrics-server 
+	$(KC) helm upgrade --install --namespace $(L2_NAMESPACE) metrics-server stable/metrics-server
 
 metrics-remove:
-	$(KC) helm del --purge metrics-server
+	$(KC) helm delete metrics-server
 
 .PHONY: .PHONY metrics-install metrics-remove
 
 prometheus-install:
-	$(KC) helm install --namespace $(L2_NAMESPACE) stable/prometheus -f $(L2DIR)/prometheus/values.yml -n prometheus \
+	$(KC) helm upgrade --install --namespace $(L2_NAMESPACE) prometeus stable/prometheus -f $(L2DIR)/prometheus/values.yml \
 		--set "server.ingress.hosts={metrics.$(L2_DOMAIN)}"
 
 prometheus-remove:
-	$(KC) helm del --purge prometheus
+	$(KC) helm delete prometheus
 
 .PHONY: .PHONY prometheus-install prometheus-remove 
 
 grafana-install:
-	$(KC) helm install --namespace $(L2_NAMESPACE) stable/grafana -f $(L2DIR)/grafana/values.yml -n grafana \
+	$(KC) helm upgrade --install --namespace $(L2_NAMESPACE) grafana stable/grafana -f $(L2DIR)/grafana/values.yml \
 		--set "ingress.hosts={status.$(L2_DOMAIN)}"
 
 grafana-remove:
-	$(KC) helm del --purge grafana
+	$(KC) helm delete grafana
 
 
 .PHONY: .PHONY grafana-install grafana-remove
